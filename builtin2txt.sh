9e2288b739a82d6ad69d961d05a836f0db73a260
Beyang Liu <beyang@sourcegraph.com>
2020-05-03 20:28:50 -0700

    doc dump scripts

---
+++ builtin2txt.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+# Rough script to dump builtin help docs as text files
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+# Generate builtins
+BUILTINS=(
+'alias'
+'bg'
+# 'bind'
+'break'
+'builtin'
+'caller'
+'cd'
+'command'
+'compgen'
+'complete'
+'compopt'
+'continue'
+'declare'
+'dirs'
+'disown'
+'echo'
+'enable'
+'eval'
+'exec'
+# 'exit'
+'export'
+'fc'
+'fg'
+'getopts'
+'hash'
+'help'
+'history'
+'jobs'
+'kill'
+'let'
+# 'local'
+'logout'
+'mapfile'
+'popd'
+'printf'
+'pushd'
+'pwd'
+'read'
+'readarray'
+'readonly'
+'return'
+'set'
+'shift'
+'shopt'
+'source'
+'test'
+'times'
+'trap'
+'type'
+'typeset'
+'ulimit'
+'umask'
+'unalias'
+'unset'
+'wait'
+)
+OUT_DIR="mandump/builtin"
+mkdir -p $OUT_DIR
+for BUILTIN in ${BUILTINS[@]}; do
+    echo $BUILTIN
+    bash -c "$BUILTIN --help" > "$OUT_DIR/$BUILTIN.txt"
+done
+INTERACTIVE_BUILTINS=(
+    'bind'
+    'exit'
+)
+for BUILTIN in ${INTERACTIVE_BUILTINS[@]}; do
+    echo $BUILTIN
+    bash -i -c "$BUILTIN --help" > "$OUT_DIR/$BUILTIN.txt"
+done
+# TODO: local
---
+++ man2txt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Rough script to dump man pages as text files
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+MAN_FILES="$(ls /usr/share/man/man1 | grep '^[a-z]*\.1\.gz$')"
+OUT_DIR="mandump/man1"
+for FILE in $MAN_FILES; do
+    echo $FILE
+    base="$(basename $FILE .1.gz)"
+    # gunzip -c "/usr/share/man/man1/$FILE" | groff -mandoc -Tascii > "$OUT_DIR/$base.txt"
+    man "$base" > "$OUT_DIR/$base.txt"
+done
+
+MAN_FILES="$(ls /usr/share/man/man8 | grep '^[a-z]*\.8\.gz$')"
+OUT_DIR="mandump/man8"
+for FILE in $MAN_FILES; do
+    echo $FILE
+    base="$(basename $FILE .8.gz)"
+    # gunzip -c "/usr/share/man/man8/$FILE" | groff -mandoc -Tascii > "$OUT_DIR/$base.txt"
+    man "$base" > "$OUT_DIR/$base.txt"
+done
